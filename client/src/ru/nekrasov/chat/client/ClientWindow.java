2ad388d55f14d8ce4e50207f83c10519ccdcbeee
nekvov123 <nekvov123@gmail.com>
2020-09-17 23:05:19 +0300

    добавить бы картинку на фон...

---
+++ client/src/ru/nekrasov/chat/client/ClientWindow.java
@@ -0,0 +1,114 @@
+package ru.nekrasov.chat.client;
+
+import ru.nekrasov.network.TCPConnection;
+import ru.nekrasov.network.TCPConnectionListener;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ClientWindow extends JFrame implements ActionListener, TCPConnectionListener {
+    private static final String IP_ADDR = "127.0.0.1";
+    private static final int PORT = 8189;
+    private static final int WIDTH = 600;
+    private static final int HEIGHT = 400;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("CLIENT NAME");
+    private final JTextField fieldInput = new JTextField();
+
+    private TCPConnection connection;
+
+    private ClientWindow() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+
+        fieldInput.addActionListener(this);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+
+        setVisible(true);
+        try {
+            connection = new TCPConnection(this, IP_ADDR, PORT);
+        } catch (IOException e) {
+            printMsg("Connection exception " + e);
+
+        }
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String msg = fieldInput.getText();
+        if (msg.equals("")) return;
+        fieldInput.setText(null);
+        connection.sendString(fieldNickname.getText() + ": " + msg);
+
+    }
+
+    @Override
+    public void onConnectionReady(TCPConnection tcpConnection) {
+        printMsg("Connection Ready...");
+    }
+
+    @Override
+    public void onReceiveString(TCPConnection tcpConnection, String value) {
+        printMsg(value);
+    }
+
+    @Override
+    public void onDisconnect(TCPConnection tcpConnection) {
+        printMsg("Connection Close...");
+    }
+
+    @Override
+    public void onException(TCPConnection tcpConnection, Exception e) {
+        printMsg("Connection exception " + e);
+    }
+
+    private synchronized void printMsg(String msg) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                log.append(msg + "\n");
+                log.setCaretPosition(log.getDocument().getLength());
+
+            }
+        });
+    }
+
+
+        public void paintComponent(Graphics g) {
+            BufferedImage image;
+            image = null;
+            try {
+                image = ImageIO.read (new File("/Изображения/38598.jpg"));
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            g.drawImage(image, 0, 0, this);
+
+        }
+    }
